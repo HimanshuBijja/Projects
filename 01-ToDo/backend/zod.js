502178bf18558c655e40ea4d3717301b73cce491
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-03-13 11:59:17 +0530

    input validation

---
+++ 01-ToDo/backend/zod.js
@@ -0,0 +1,17 @@
+const zod = require("zod");
+
+const todoSchema = zod.object({
+    title : zod.string(),
+    description : zod.string()
+});
+
+const updateSchema = zod.object({
+    id : zod.string()
+})
+const id = zod.string();
+
+module.exports = {
+    todoSchema,
+    updateSchema,
+    id
+}
