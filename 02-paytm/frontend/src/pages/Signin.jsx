e171aaa1cd007120f0673465e2572c19709f7b80
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-05-08 23:13:32 +0530

    commit

--- 02-paytm/frontend/src/pages/Signin.jsx
+++ 02-paytm/frontend/src/pages/Signin.jsx
@@ -4,6 +4,7 @@ import { Heading } from "../components/Heading";
 import { InputBox } from "../components/InputBox";
 import { SubHeading } from "../components/SubHeading";
 import { BottomWarning} from "../components/BottomWarning";
+import { Link } from "react-router-dom"
 
 export function Signin() {
 	return (
@@ -15,7 +16,9 @@ export function Signin() {
 			<InputBox label={"Username"} placeholder={"abc@gmail.com"} />
 			<InputBox label={"Password"} placeholder={"Xyz"} />
 			<div className="px-1 mt-4 mb-2">
+			<Link to={"/dashboard"}>
 				<Button label={"Sign in"}/>
+			</Link>
 			</div>
             <BottomWarning label={"Dont have an account?  "} buttonText={"Sign up"} to={"/signup"}/>
 		</Background>
